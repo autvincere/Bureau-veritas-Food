708c848a5f2d951b69e05179a5303faa2070a171
autvincere <aut.vincere@gmail.com>
2016-03-09 11:24:14 -0300

    gulp configuration finished
    configuración de modelos y gulpfile.js lista!!

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,20 @@ var sass = require('gulp-sass')
 var autoprefixer = require('gulp-autoprefixer')
 var uglify = require('gulp-uglify')//minifica js
 var cleanCSS = require('gulp-clean-css');//minifica css
+var cssScss = require('gulp-css-scss');// css to sass
 
 /*Tareas*/
-//.css
+
+//css to sass 
+gulp.task('css-scss', () => {
+  return gulp.src('css/style.css')//convierte archivo especifico
+    .pipe(cssScss())
+    .pipe(gulp.dest('css/'));
+});
+
+//sass to .css and minify
 gulp.task('sass', function () {
-    return gulp.src('css/*.scss')
+    return gulp.src('css/*.scss')//convierte todos los archivos que terminen en .scss
         .pipe(sass())
        .pipe(autoprefixer())
 //.pipe(cleanCSS({compatibility: 'ie8'}))
@@ -17,7 +26,7 @@ gulp.task('sass', function () {
 
 //.js
 gulp.task('compress', function() {
-   return gulp.src('js/funciones_belleza.js')
+   return gulp.src('js/funciones.js')//convierte archivo especifico
 //  return gulp.src('js/'+ archivos)
     .pipe(uglify())
     .pipe(gulp.dest('js/min/'));
