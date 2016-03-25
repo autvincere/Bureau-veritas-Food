8e6d030e280fff1806ed73f0a0dd0c819693e382
autvincere <aut.vincere@gmail.com>
2016-03-25 17:55:47 -0300

    slider

--- js/funciones.js
+++ js/funciones.js
@@ -1,36 +1,40 @@
 $(document).ready(function () {
 
-    //slides js
-    $(function () {
-        $('#slides').slidesjs({
-            width: 940,
-            height: 528,
-            play: {
-                active: false,
-                // [boolean] Generate the play and stop buttons.
-                // You cannot use your own buttons. Sorry.
-                interval: 4000,
-                // [number] Time spent on each slide in milliseconds.
-                auto: true,
-                // [boolean] Start playing the slideshow on load.
-                swap: true,
-                // [boolean] show/hide stop and play buttons
-                pauseOnHover: true,
-                // [boolean] pause a playing slideshow on hover
-                restartDelay: 2500
-                    // [number] restart delay on inactive slideshow
-            },
-            pagination: {
-                effect: "slide"
-            }, // slide o fade
-
-            effect: {
-                slide: {
-                    speed: 2000,
-                }
-            } // velocidad de la transicion
-        });
+    $('.flexslider').flexslider({
+        animation: "slide"
     });
+
+    //slides js
+    //    $(function () {
+    //        $('#slides').slidesjs({
+    //            width: 940,
+    //            height: 528,
+    //            play: {
+    //                active: false,
+    //                // [boolean] Generate the play and stop buttons.
+    //                // You cannot use your own buttons. Sorry.
+    //                interval: 4000,
+    //                // [number] Time spent on each slide in milliseconds.
+    //                auto: true,
+    //                // [boolean] Start playing the slideshow on load.
+    //                swap: true,
+    //                // [boolean] show/hide stop and play buttons
+    //                pauseOnHover: true,
+    //                // [boolean] pause a playing slideshow on hover
+    //                restartDelay: 2500
+    //                    // [number] restart delay on inactive slideshow
+    //            },
+    //            pagination: {
+    //                effect: "slide"
+    //            }, // slide o fade
+    //
+    //            effect: {
+    //                slide: {
+    //                    speed: 2000,
+    //                }
+    //            } // velocidad de la transicion
+    //        });
+    //    });
     //jQuery to collapse the navbar on scroll
     $(window).scroll(function () {
         if ($(".navbar").offset().top > 50) {
