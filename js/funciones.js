1ee3a58b19642c57ff11d3edf119f8506cf2cabc
autvincere <aut.vincere@gmail.com>
2016-03-30 01:08:07 -0300

    ajuste slider

--- js/funciones.js
+++ js/funciones.js
@@ -4,43 +4,23 @@ $(document).ready(function () {
         animation: "slide"
     });
 
-    //slides js
-    //    $(function () {
-    //        $('#slides').slidesjs({
-    //            width: 940,
-    //            height: 528,
-    //            play: {
-    //                active: false,
-    //                // [boolean] Generate the play and stop buttons.
-    //                // You cannot use your own buttons. Sorry.
-    //                interval: 4000,
-    //                // [number] Time spent on each slide in milliseconds.
-    //                auto: true,
-    //                // [boolean] Start playing the slideshow on load.
-    //                swap: true,
-    //                // [boolean] show/hide stop and play buttons
-    //                pauseOnHover: true,
-    //                // [boolean] pause a playing slideshow on hover
-    //                restartDelay: 2500
-    //                    // [number] restart delay on inactive slideshow
-    //            },
-    //            pagination: {
-    //                effect: "slide"
-    //            }, // slide o fade
-    //
-    //            effect: {
-    //                slide: {
-    //                    speed: 2000,
-    //                }
-    //            } // velocidad de la transicion
-    //        });
-    //    });
-    //jQuery to collapse the navbar on scroll
+    //flexslider
     $(window).scroll(function () {
-        if ($(".navbar").offset().top > 50) {
+        var w = window.innerWidth;
+
+        if ($(".navbar").offset().top >= 80) {
             $(".navbar-fixed-top").addClass("top-nav-collapse");
-        } else {
+            $("img#movil").fadeIn();;
+            $("img#desktop").hide();
+//        }else if
+//            ($(".navbar").offset().top < 80 & w >= 767){ 
+//            $(".navbar-fixed-top").addClass("top-nav-collapse");
+//            $("img#movil").fadeIn();;
+//            $("img#desktop").hide();  
+        }else{
             $(".navbar-fixed-top").removeClass("top-nav-collapse");
+            $("img#movil").hide();
+            $("img#desktop").fadeIn();;
         }
     });
 
@@ -54,6 +34,16 @@ $(document).ready(function () {
             event.preventDefault();
         });
     });
+    
+    // hover servicios
+      $('.thumbnail').hover(
+        function(){
+            $(this).find('.caption').slideDown(250); //.fadeIn(250)
+        },
+        function(){
+            $(this).find('.caption').slideUp(250); //.fadeOut(205)
+        }
+    ); 
 
 
 });
